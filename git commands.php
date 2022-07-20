43b6e2b70040f5b0cce226ce89727d78d523c1bf
Pooja <poojamaurya.allalgos@gmail.com>
2022-07-20 21:34:55 +0530

    file pushed

---
+++ git commands.php
@@ -0,0 +1,44 @@
+var =>globle scoped
+can redclared,reassigned valu
+
+yes js is case sensitive,some of varible,function keyword are case sentive
+
+
+
+
+Git Commands
+
+1.	To add files to queue to commit 
+	to make file ready for commit
+		  git add .  (all files)
+
+		  git add filename (single file)
+
+2. Commit file to push
+
+	
+	git commit -m "updates/changes"
+
+3.	Git push
+
+	 git push origin branchname
+
+4. files me modification/conflict status 
+	
+	git status
+
+5. get all new branches
+
+	git fetch --all
+
+6. merge 2 branches
+
+	git merge branchname
+
+7. pull recent changes 
+
+	git pull origin branchname
+
+8. clone any repository
+
+	git clone repo link
